f6668eb3105020dc1ed428869277c7cded0ee95d
ARMANDA2022 <104578464+ARMANDA2022@users.noreply.github.com>
2024-08-14 16:35:50 +0000

    terminado

--- src/app.py
+++ src/app.py
@@ -1,52 +1,35 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-class FamilyStructure:
-    def __init__(self, last_name):
-        self.last_name = last_name
-        self.next_id = 1
-        self.members = []
+import os
 
-
-from flask import Flask, jsonify; request
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from utils import APIException, generate_sitemap
+from datastructures import FamilyStructure
 #from models import Person
 
 app = Flask(__name__)
-# app.url_map.strict_slashes = False
-# CORS(app)
+app.url_map.strict_slashes = False
+CORS(app)
 
 # create the jackson family object, Inicializa la clase FamilyStructure con el nombre 'Jackson'
 jackson_family = FamilyStructure('Jackson')
 
+# Handle/serialize errors like a JSON object
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
 
-# Agregar los miembros iniciales de la familia Jackson
-jackson_family.add_member({
-    "first_name": "John",
-    "age": 33,
-    "lucky_numbers": [7, 13, 22]
-})
-
-jackson_family.add_member({
-    "first_name": "Jane",
-    "age": 35,
-    "lucky_numbers": [10, 14, 3]
-})
-
-jackson_family.add_member({
-    "first_name": "Jimmy",
-    "age": 5,
-    "lucky_numbers": [1]
-})
+# generate sitemap with all your endpoints
+@app.route('/')
+def sitemap():
+    return generate_sitemap(app)
 
 
 #declaro los Endpoints
 
 # Aqui con este Endpoints 'GET' devuelvo, obtengo todos los miembros de la familia
-@app.route('/members', methods=['GET'])
-def get_all_members():
-    members = jackson_family.get_all_members()
-    return jsonify(members), 200
-
 
 # En este Endpoints 'GET' Obtengo un miembro específico por su ID
 @app.route('/members', methods=['GET'])
@@ -66,7 +49,7 @@ def get_member(id):
     
 
     # En este Endpoints 'POST' Añado un nuevo miembro
-    @app.route('/member', methods=['POST'])
+@app.route('/member', methods=['POST'])
 def add_member():
     member = request.json
     jackson_family.add_member(member)
--- src/datastructures.py
+++ src/datastructures.py
@@ -8,22 +8,63 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+#iniciazlizando la extrutura de datos. inicializamos la extructura de la clase desde el componente que tiene los objetos
+class FamilyStructure: 
+    def __init__(self, last_name):
+        self.last_name = last_name
+#Aqui le damos valor cero al self._next_ id, para que empieze la apsinacion de id desde cero 
+        self._next_id = 0
 
+ #aqui escribimos todos los objetos que se vera en pantalla
+        self._members =[
+        {
+            "first_name":"john",
+            "last_name": self.last_name,
+            "age": 33,
+            "lucky_numbers": [7,13,22],
+            "id": self._generate_id()
+        }, 
+        {
+            "first_name":"Jane ",
+            "last_name": self.last_name,
+            "age": 35,
+            "lucky_numbers": [10, 14, 3],
+            "id": self._generate_id()
 
-    
+        },
+        {
+            "first_name":"Jimmy ",
+            "last_name": self.last_name,
+            "age": 5,
+            "lucky_numbers": [1],
+            "id": self._generate_id()
+
+        }
+
+        ]
+        
+    #creamos la variable def_generate_id y le introducimos el parametro self
      # Este método genera un 'id' único al agregar miembros a la lista 
-    def _age_id(self, age):
+    def _generate_id(self): 
+ #a continuacion la ponemos la variable creada a igual self.nombre_id              
         generated_id = self._next_id
+ #Aqui le asignamos la operacion matematica += 1 Esto es para que vaya sumando los id de los objetos segun lo vas introducien en uno mas     
         self._next_id += 1
         return generated_id
-
+    
+ # Asigna un ID único al miembro  
     def add_member(self, member):
-        member['id'] = self._generate_id('jackson')  # Asigna un ID único al miembro
-        member['last_name'] = self.last_name  # Asegura que el apellido sea 'Jackson'
+        member['id'] = self._generate_id() 
+# Asegura que el apellido sea 'Jackson'         
+        member['last_name'] = self.last_name  
         self._members.append(member)
+        return member
 
-    def delete_member(self, name):
-        self._members = [member for member in self._members if member['id'] != id']
+    def delete_member(self, id):
+        for member in self._members:
+            if member['id'] == id: 
+                self._members.remove(member)
+                return {"done":True}
 
     def get_member(self, id_numbers):
         for member in self._members:
